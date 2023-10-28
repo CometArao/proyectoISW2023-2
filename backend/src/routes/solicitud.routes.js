a55ddefda6e920affb6d753f0fab94a15a79be14
Drixity360 <fernanda.morales2001@alumnos.ubiobio.cl>
2023-10-28 20:17:05 -0300

    actualizado

--- backend/src/routes/solicitud.routes.js
+++ backend/src/routes/solicitud.routes.js
@@ -3,16 +3,17 @@
  const express = require("express");
  const { isAdmin } = require("../middlewares/authorization.middleware.js");
 // Importa el middleware de autenticación
-const authentication= require("../middlewares/authentication.middleware.js");
+const verifyJWT= require("../middlewares/authentication.middleware.js");
 /** Controlador de solicitudes */
 const solicitudController = require("../controllers/solicitud.controller.js");
 /** Instancia del encrutador */
-const router = express.Router(authentication);
+const router = express.Router();
 
 // Define las rutas para  solicitudes que solo ven los usuarios
 router.get("/", solicitudController.getSolicitudes);
 router.post("/", solicitudController.createSolicitud);
 // Rutas para administrador
+router.use(verifyJWT);
 router.get("/:id", isAdmin, solicitudController.getSolicitudById);
 router.put("/:id", isAdmin, solicitudController.updateEstado);
 router.delete("/:id", isAdmin, solicitudController.deleteSolicitud);
