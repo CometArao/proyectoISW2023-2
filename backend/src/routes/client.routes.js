ddad54fea8023a5567e985d69697d62aed0e8952
Thitroy <90158022+Thitroy@users.noreply.github.com>
2023-10-28 21:37:39 -0300

    Solicitud (#13)
    * creacion para la gestion de solicitud
    
    * avance en requisitos funcionales de cliente y solicitud
    
    * actualizado

---
+++ backend/src/routes/client.routes.js
@@ -0,0 +1,22 @@
+"use strict";
+// Importa el modulo 'express' para crear las rutas
+const express = require("express");
+// Importa el middleware de autorización 
+const { isAdmin } = require("../middlewares/authorization.middleware.js");
+// Importa el middleware de autenticación
+const verifyJWT= require("../middlewares/authentication.middleware.js");
+/** Controlador de clientes */
+const clienteController = require("../controllers/client.controller.js");
+/** Instancia del encrutador */
+const router = express.Router();
+
+router.use(verifyJWT);
+// Accesible solo por administradores
+ router.get("/", isAdmin, clienteController.getClientes);
+router.post("/", isAdmin, clienteController.createCliente);
+router.get("/:id", isAdmin, clienteController.getClientesById);
+ router.put("/:id", isAdmin, clienteController.updateClientesById);
+router.delete("/:id", isAdmin, clienteController.deleteClientesById);
+
+// Exporta el enrutador
+module.exports = router;
