4b4ebdd3d54fd695e0ad7e9ad97163d4d0e374c3
Francisco Barraza <francisco.barraza2001@alumnos.ubiobio.cl>
2023-12-18 01:46:57 -0300

    se agregan rutas a comunas y regiones

---
+++ backend/src/controllers/location.controller.js
@@ -0,0 +1,123 @@
+"use strict";
+
+const { respondSuccess, respondError } = require("../utils/resHandler");
+const LocationService = require("../services/location.service");
+const { handleError } = require("../utils/errorHandler");
+// const { Region, Commune }= require("../models/location.model.js");
+const { locationIdSchema } = require("../schema/location.schema");
+
+/**
+ * Obtiene todas las regiones
+ * @param {Object} req - Objeto de petición
+ * @param {Object} res - Objeto de respuesta
+ */
+async function getRegions(req, res) {
+    try {
+        const [regions, errorRegions] = await LocationService.getRegions();
+        if (errorRegions) return respondError(req, res, 404, errorRegions);
+
+        regions.length === 0
+            ? respondSuccess(req, res, 204)
+            : respondSuccess(req, res, 200, regions);
+    } catch (error) {
+        handleError(error, "location.controller -> getRegions");
+        respondError(req, res, 400, error.message);
+    }
+}
+
+/**
+ * Obtiene todas las comunas
+ * @param {Object} req - Objeto de petición
+ * @param {Object} res - Objeto de respuesta
+ */
+async function getCommunes(req, res) {
+    try {
+        const [communes, errorCommunes] = await LocationService.getCommunes();
+        if (errorCommunes) return respondError(req, res, 404, errorCommunes);
+
+        communes.length === 0
+            ? respondSuccess(req, res, 204)
+            : respondSuccess(req, res, 200, communes);
+    } catch (error) {
+        handleError(error, "location.controller -> getCommunes");
+        respondError(req, res, 400, error.message);
+    }
+}
+
+/**
+ * Obtiene una ubicación por su ID.
+ * @param {Object} req - Objeto de petición
+ * @param {Object} res - Objeto de respuesta
+ */
+async function getLocationById(req, res) {
+    try {
+        const { params } = req;
+        const { error: paramsError } = locationIdSchema.validate(params);
+        if (paramsError) return respondError(req, res, 400, paramsError.message);
+
+        let location;
+        let error;
+        if (params.id) {
+            [location, error] = await LocationService.getRegionById(params.id);
+            if (!location) {
+                [location, error] = await LocationService.getCommuneById(params.id);
+            }
+        }
+
+        if (error) return respondError(req, res, 404, error);
+
+        respondSuccess(req, res, 200, location);
+    } catch (error) {
+        handleError(error, "location.controller -> getLocationById");
+        respondError(req, res, 400, error.message);
+    }
+}
+
+// /**
+//  * Obtiene una región por su ID
+//  * @param {Object} req - Objeto de petición
+//  * @param {Object} res - Objeto de respuesta
+//  */
+// async function getRegionById(req, res) {
+//     try {
+//         const { params } = req;
+//         const { error: paramsError } = locationIdSchema.validate(params);
+//         if (paramsError) return respondError(req, res, 400, paramsError.message);
+//         const [region, errorRegion] = await LocationService.getRegionById(params.id);
+//         if (errorRegion) return respondError(req, res, 404, errorRegion);
+
+//         respondSuccess(req, res, 200, region);
+//     } catch (error) {
+//         handleError(error, "location.controller -> getRegionById");
+//         respondError(req, res, 400, error.message);
+//     }
+// }
+
+// /**
+//  * Obtiene una comuna por su ID
+//  * @param {Object} req - Objeto de petición
+//  * @param {Object} res - Objeto de respuesta
+//  */
+// async function getCommuneById(req, res) {
+//     try {
+//         const { params } = req;
+//         const { error: paramsError } = locationIdSchema.validate(params);
+//         if (paramsError) return respondError(req, res, 400, paramsError.message);
+
+//         const [commune, errorCommune] = await LocationService.getCommuneById(params.id);
+//         if (errorCommune) return respondError(req, res, 404, errorCommune);
+
+//         respondSuccess(req, res, 200, commune);
+//     } catch (error) {
+//         handleError(error, "location.controller -> getCommuneById");
+//         respondError(req, res, 400, error.message);
+//     }
+// }
+
+module.exports = {
+    getRegions,
+    getCommunes,
+    getLocationById,
+    // getRegionById,
+    // getCommuneById,
+};
---
+++ backend/src/services/location.service.js
@@ -0,0 +1,71 @@
+"use strict";
+
+const { Region, Commune }= require("../models/location.model.js");
+const { handleError } = require("../utils/errorHandler");
+
+/**
+ * Get all regions.
+ *
+ * @returns {Promise<[Array<Region> | null, string | null]>} The regions or an error message.
+ */
+/**
+ * Get all regions.
+ *
+ * @returns {Promise<[Array<Region> | null, string | null]>} The regions or an error message.
+ */
+async function getRegions() {
+    try {
+        const regions = await Region.find();
+        if (!regions) return [null, "No hay regiones"];
+
+        return [regions, null];
+    } catch (error) {
+        handleError(error, "location.service -> getRegions");
+    }
+}
+
+/**
+ * Get all communes.
+ *
+ * @returns {Promise<[Array<Commune> | null, string | null]>} The communes or an error message.
+ */
+async function getCommunes() {
+    try {
+        const communes = await Commune.find();
+        if (!communes) return [null, "No hay comunas"];
+
+        return [communes, null];
+    } catch (error) {
+        handleError(error, "location.service -> getCommunes");
+    }
+}
+
+async function getRegionById(id) {
+    try {
+        console.log("id", id);
+        const region = await Region.findById(id);
+        if (!region) return [null, "No existe la región"];
+
+        return [region, null];
+    } catch (error) {
+        handleError(error, "location.service -> getRegionById");
+    }
+}
+
+async function getCommuneById(id) {
+    try {
+        const commune = await Commune.findById(id);
+        if (!commune) return [null, "No existe la comuna"];
+
+        return [commune, null];
+    } catch (error) {
+        handleError(error, "location.service -> getCommuneById");
+    }
+}
+
+module.exports = {
+    getRegions,
+    getCommunes,
+    getRegionById,
+    getCommuneById,
+};
