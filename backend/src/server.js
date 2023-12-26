5a8c0f52aab8aaf45aa7467838acda2735254597
Francisco Barraza <francisco.barraza2001@alumnos.ubiobio.cl>
2023-12-26 12:48:06 -0300

    ruta a imagenes modificada

--- backend/src/server.js
+++ backend/src/server.js
@@ -49,8 +49,8 @@ async function setupServer() {
     server.use(morgan("dev"));
 
     // Configuración de rutas estáticas para las imágenes
-    server.use("/images", express.static(path.join(__dirname, "./data/images")));
-    // server.use("api/images", express.static(path.join(__dirname, "./data/images")));
+    // server.use("/images", express.static(path.join(__dirname, "./data/images")));
+    server.use("/api/images", express.static(path.join(__dirname, "./data/images")));
     // console.log("dirname: ", __dirname);
     
     // Agrega el middleware para el manejo de datos en formato URL
