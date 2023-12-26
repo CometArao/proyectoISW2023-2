b73c73e0679ae98a73b844d75bf8cc580b32e065
frenubb <143917255+frenubb@users.noreply.github.com>
2023-12-26 13:21:25 -0300

    Delete frontend directory

--- frontend/src/lib/getAgreements.js
+++
@@ -1,8 +0,0 @@
-import axios from "../services/root.service.js";
-
-export async function getData(){
-    const res = await axios.get(`/convenios`);
-    const data = res.data.data
-    console.log('Data from API:', data);
-    return data;
-}
